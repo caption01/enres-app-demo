5cb16d4af4107c3bd0f42e7467a9feddc67f0c05
nutchapon.h <nutchapon@enres.com>
2020-10-21 18:21:26 +0700

    fix index page

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,18 +1,30 @@
 import React from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const IndexScreen = () => {
   const navigation = useNavigation();
 
   return (
-    <View>
-      <Text>Enres Application.</Text>
-      <Button title="Demo" onPress={() => navigation.navigate("Tabs")} />
+    <View style={styles.page}>
+      <TouchableOpacity onPress={() => navigation.navigate("Tabs")}>
+        <Text style={styles.text}>Enres Application.</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    borderColor: "red",
+    borderWidth: 2,
+  },
+  text: {
+    fontSize: 38,
+  },
+});
 
 export default IndexScreen;
